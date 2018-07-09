2a0c6e490c732db2d0d91c3728d187f31162c1b0
Leonard Martin <leonard.martin@gmail.com>
2018-07-09 10:39:29 +0100

    Refactor to consume message from SQS
    Instead of running a server and responding to inbound http requests, the resolver should listen to the SQS queue, and resolve messages it finds.

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
   port: process.env.PORT || 8080,
+  sqs: {
+    region: process.env.SQS_REGION || 'eu-west-2',
+    accessKey: process.env.SQS_ACCESS_KEY,
+    secret: process.env.SQS_SECRET,
+    url: process.env.SQS_URL
+  },
   db: {
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
