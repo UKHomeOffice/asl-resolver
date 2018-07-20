e4709a3de999a1e94d28eb130a28b7c61161a611
Leonard Martin <leonard.martin@gmail.com>
2018-07-20 15:53:07 +0100

    Spike of user account creation

--- config.js
+++ config.js
@@ -12,5 +12,13 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME
+  },
+  auth: {
+    realm: process.env.KEYCLOAK_REALM,
+    url: process.env.KEYCLOAK_URL,
+    client: process.env.KEYCLOAK_CLIENT,
+    secret: process.env.KEYCLOAK_SECRET,
+    username: process.env.KEYCLOAK_USERNAME,
+    password: process.env.KEYCLOAK_PASSWORD
   }
 };
