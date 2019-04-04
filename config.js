5d52068b64dda56dde62d57ab2b09870ff21a212
Joe Fitter <joefitter@hotmail.com>
2019-04-04 11:40:05 +0100

    query S3 for messages

--- config.js
+++ config.js
@@ -5,6 +5,12 @@ module.exports = {
     secret: process.env.SQS_SECRET,
     url: process.env.SQS_URL
   },
+  s3: {
+    region: process.env.S3_REGION,
+    accessKey: process.env.S3_ACCESS_KEY,
+    secret: process.env.S3_SECRET,
+    bucket: process.env.S3_BUCKET
+  },
   db: {
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
