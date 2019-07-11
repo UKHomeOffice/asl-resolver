1b325e4a5036a7d9ca8f9f1c39cf3ccb09302b09
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-11 12:36:16 +0100

    test for project resolver

---
+++ test/resolvers/project.js
@@ -0,0 +1,126 @@
+const assert = require('assert');
+const moment = require('moment');
+const { project } = require('../../lib/resolvers');
+const db = require('../helpers/db');
+
+const profileId = 'f0835b01-00a0-4c7f-954c-13ed2ef7efd9';
+const projectId = '1da9b8b7-b12b-49f3-98be-745d286949a7';
+
+describe('Project resolver', () => {
+
+  before(() => {
+    this.models = db.init();
+    this.project = project({ models: this.models });
+  });
+
+  beforeEach(() => {
+    return db.clean(this.models)
+      .then(() => this.models.Establishment.query().insert({
+        id: 8201,
+        name: 'Univerty of Croydon'
+      }))
+      .then(() => this.models.Profile.query().insert({
+        id: profileId,
+        userId: 'abc123',
+        title: 'Dr',
+        firstName: 'Linford',
+        lastName: 'Christie',
+        address: '1 Some Road',
+        postcode: 'A1 1AA',
+        email: 'test1@example.com',
+        telephone: '01234567890'
+      }))
+      .then(() => this.models.Project.query().insert([
+        {
+          id: projectId,
+          status: 'active',
+          title: 'Hypoxy and angiogenesis in cancer therapy',
+          issueDate: new Date('2019-07-11').toISOString(),
+          expiryDate: new Date('2022-07-11').toISOString(),
+          licenceNumber: 'PP-627808',
+          establishmentId: 8201,
+          licenceHolderId: profileId
+        }
+      ]))
+      .then(() => this.models.ProjectVersion.query().insert([
+        {
+          id: '8fb05730-f2ec-4d8f-8085-bbdc86937c54',
+          projectId,
+          data: {},
+          status: 'draft',
+          createdAt: new Date('2019-07-04').toISOString()
+        },
+        {
+          id: '68d79bb1-3573-4402-ac08-7ac27dcbb39e',
+          projectId,
+          data: {},
+          status: 'submitted',
+          createdAt: new Date('2019-07-03').toISOString()
+        },
+        {
+          id: 'ee871d64-cc87-470a-82d9-4a326c9c08dc',
+          projectId,
+          data: {},
+          status: 'draft',
+          createdAt: new Date('2019-07-02').toISOString()
+        },
+        {
+          id: '574266e5-ef34-4e34-bf75-7b6201357e75',
+          projectId,
+          data: {},
+          status: 'granted',
+          createdAt: new Date('2019-07-01').toISOString()
+        },
+        {
+          id: 'b497b05a-f1e0-4596-8b02-60e129e2ab49',
+          projectId,
+          data: {},
+          status: 'submitted',
+          createdAt: new Date('2019-06-04').toISOString()
+        },
+        {
+          id: '71e25eca-e0aa-4555-b09b-62f55b83e890',
+          projectId,
+          data: {},
+          status: 'granted',
+          createdAt: new Date('2019-06-03').toISOString()
+        }
+      ]));
+  });
+
+  afterEach(() => db.clean(this.models));
+
+  after(() => this.models.destroy());
+
+  describe('delete amendments', () => {
+    it('only soft deletes versions since the most recent granted version', () => {
+      const opts = {
+        action: 'delete-amendments',
+        id: projectId
+      };
+      return Promise.resolve()
+        .then(() => this.project(opts))
+        .then(() => this.models.ProjectVersion.query())
+        .then(versions => {
+          versions.map(version => {
+            if ([
+              '8fb05730-f2ec-4d8f-8085-bbdc86937c54',
+              '68d79bb1-3573-4402-ac08-7ac27dcbb39e',
+              'ee871d64-cc87-470a-82d9-4a326c9c08dc'
+            ].includes(version.id)) {
+              assert(moment(version.deleted).isValid(), 'version was soft deleted');
+            }
+
+            if ([
+              '574266e5-ef34-4e34-bf75-7b6201357e75',
+              'b497b05a-f1e0-4596-8b02-60e129e2ab49',
+              '71e25eca-e0aa-4555-b09b-62f55b83e890'
+            ].includes(version.id)) {
+              assert(!version.deleted, 'version was not deleted');
+            }
+          });
+        });
+    });
+  });
+
+});
