b7233b20a2d13a078dd366abaa0ad6cc34c85b0a
Joe Fitter <joefitter@hotmail.com>
2020-09-15 11:00:45 +0100

    Feature - expire training pils
    * Updated the expiry task to include training pils

--- tasks/expire.js
+++ tasks/expire.js
@@ -1,24 +1,35 @@
 const moment = require('moment');
 
-const expire = ({ models, logger }) => {
-  const { Project } = models;
-  const midnightLastNight = moment.utc().startOf('day').toISOString();
-  logger.info(`performing project expiry check with cutoff of ${midnightLastNight}`);
-
-  return Project.query()
+function expireModels(Model, beforeTime) {
+  return Model.query()
     .patch({ status: 'expired' })
-    .where('expiryDate', '<', midnightLastNight)
+    .where('expiryDate', '<', beforeTime)
     .where('status', 'active')
-    .returning('*')
-    .then(projects => {
-      if (projects.length > 0) {
-        projects.forEach(project => {
-          logger.info(`expired project ${project.licenceNumber}`);
-        });
-      } else {
-        logger.info('no projects found to expire');
-      }
-    });
+    .returning('*');
+}
+
+const expire = ({ models, logger }) => {
+  const { Project, TrainingPil } = models;
+  const midnightLastNight = moment.utc().startOf('day').toISOString();
+  logger.info(`performing project and cat e pil expiry check with cutoff of ${midnightLastNight}`);
+
+  function reportExpiry(models, modelName) {
+    if (models.length > 0) {
+      models.forEach(item => {
+        logger.info(`expired ${modelName} ${item.licenceNumber}`);
+      });
+    } else {
+      logger.info(`no ${modelName}s found to expire`);
+    }
+  }
+
+  return Promise.all([
+    expireModels(Project, midnightLastNight),
+    expireModels(TrainingPil, midnightLastNight)
+  ]).then(([projects, trainingPils]) => {
+    reportExpiry(projects, 'project');
+    reportExpiry(trainingPils, 'trainingPil');
+  });
 };
 
 module.exports = expire;
