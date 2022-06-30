bb3f76a501bebe248ce026f8aa2ba6f7bea63a4b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-06-30 12:19:29 +0100

    pil condition reminder tests

--- test/resolvers/pil.js
+++ test/resolvers/pil.js
@@ -38,6 +38,15 @@ const LICENSING = {
   asruLicensing: true
 };
 
+const CONDITIONS_PIL = {
+  id: '92bec570-ca04-47b2-bafc-43e95fb7f564',
+  profileId: PILH.id,
+  establishmentId: 8201,
+  status: 'active',
+  licenceNumber: 'XYZ-987',
+  procedures: ['A']
+};
+
 function isThenish(date, expected) {
   return moment(date).isBetween(moment(expected).subtract(5, 'seconds'), moment(expected).add(5, 'seconds'));
 }
@@ -420,6 +429,124 @@ describe('PIL resolver', () => {
       });
     });
 
+  });
+
+  describe('Update conditions', () => {
+    beforeEach(() => {
+      return this.models.PIL.query().insert(CONDITIONS_PIL);
+    });
+
+    it('can update the existing conditions', () => {
+      const opts = {
+        action: 'update-conditions',
+        id: CONDITIONS_PIL.id,
+        data: {
+          conditions: 'Some new conditions'
+        }
+      };
+      return Promise.resolve()
+        .then(() => this.pil(opts))
+        .then(() => this.models.PIL.query().findById(opts.id))
+        .then(pil => {
+          assert.deepEqual(pil.conditions, 'Some new conditions', 'the conditions should be updated');
+        });
+    });
+
+    it('can save a new condition reminder', () => {
+      const opts = {
+        action: 'update-conditions',
+        id: CONDITIONS_PIL.id,
+        data: {
+          conditions: 'Some new conditions',
+          reminder: {
+            deadline: '2022-07-01'
+          }
+        }
+      };
+
+      return Promise.resolve()
+        .then(() => this.pil(opts))
+        .then(() => this.models.Reminder.query().where({ modelType: 'pil', modelId: opts.id }))
+        .then(reminders => {
+          assert.deepEqual(reminders.length, 1, 'there should be a single reminder');
+          assert.deepEqual(reminders[0].deadline, '2022-07-01', 'the deadline should be correct');
+          assert.deepEqual(reminders[0].status, 'active', 'the status should be active');
+          assert.deepEqual(reminders[0].deleted, null, 'the deleted column should be null');
+        });
+    });
+
+    it('can update an existing condition reminder', () => {
+      const reminder = {
+        deadline: '2022-07-03',
+        modelType: 'pil',
+        modelId: CONDITIONS_PIL.id,
+        establishmentId: CONDITIONS_PIL.establishmentId,
+        status: 'active'
+      };
+
+      return this.models.Reminder.query().insert(reminder).returning('id')
+        .then(reminder => {
+          const opts = {
+            id: CONDITIONS_PIL.id,
+            action: 'update-conditions',
+            data: {
+              conditions: 'Some new conditions',
+              reminder: {
+                id: reminder.id,
+                deadline: '2022-11-22'
+              }
+            }
+          };
+
+          return Promise.resolve()
+            .then(() => this.pil(opts))
+            .then(() => this.models.Reminder.query().where({ modelType: 'pil', modelId: opts.id }))
+            .then(reminders => {
+              assert.deepEqual(reminders.length, 1, 'there should be a single reminder');
+              assert.deepEqual(reminders[0].deadline, '2022-11-22', 'the deadline should be updated');
+              assert.deepEqual(reminders[0].status, 'active', 'the status should still be active');
+              assert.deepEqual(reminders[0].deleted, null, 'the deleted column should be null');
+            });
+        });
+    });
+
+    it('can delete an existing condition reminder', () => {
+      const reminder = {
+        deadline: '2022-07-03',
+        modelType: 'pil',
+        modelId: CONDITIONS_PIL.id,
+        establishmentId: CONDITIONS_PIL.establishmentId,
+        status: 'active'
+      };
+
+      return this.models.Reminder.query().insert(reminder).returning('id')
+        .then(reminder => {
+          const opts = {
+            id: CONDITIONS_PIL.id,
+            action: 'update-conditions',
+            data: {
+              conditions: 'Some new conditions',
+              reminder: {
+                id: reminder.id,
+                deleted: true
+              }
+            }
+          };
+
+          return Promise.resolve()
+            .then(() => this.pil(opts))
+            .then(() => this.models.Reminder.query().where({ modelType: 'pil', modelId: opts.id }))
+            .then(reminders => {
+              assert.deepEqual(reminders.length, 0, 'there should be no reminders returned in the standard query');
+            })
+            .then(() => this.models.Reminder.queryWithDeleted().where({ modelType: 'pil', modelId: opts.id }))
+            .then(reminders => {
+              assert.deepEqual(reminders.length, 1, 'there should be a single deleted reminder');
+              assert.ok(reminders[0].deleted, 'the deleted column should be set');
+              assert(moment(reminders[0].deleted).isValid(), 'deleted date is a valid date');
+            });
+        });
+    });
   });
 
 });
