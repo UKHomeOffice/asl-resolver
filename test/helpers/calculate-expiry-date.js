955e18239c7822af3673f650bf6057a37173a8d4
Kiran Varma <Kiran.Varma@digital.homeoffice.gov.uk>
2024-08-03 00:22:35 +0100

    expiry working in db, calc test fixed

--- test/helpers/calculate-expiry-date.js
+++ test/helpers/calculate-expiry-date.js
@@ -1,27 +1,38 @@
 const assert = require('assert');
 const moment = require('moment');
+const sinon = require('sinon');
 
 const calculateExpiryDate = require('../../lib/helpers/calculate-expiry-date.js');
 
 describe('calculateExpiryDate', () => {
 
+    let clock;
+
+    beforeEach(() => {
+        clock = sinon.useFakeTimers(new Date('2024-01-01'));
+    });
+
+    afterEach(() => {
+        clock.restore();
+    });
+
     it('should expire license in 1 year after issue date', () => {
         const result = calculateExpiryDate(new Date('2014-07-26').toISOString(), moment.duration(1, 'year'));
-        assert.equal(result, '2015-07-25T23:59:59.999+01:00');
+        assert.equal(result, '2015-07-25T23:59:59.999Z');
     });
 
     it('should expire license in 2 days from given date', () => {
         const result = calculateExpiryDate(undefined, moment.duration(2, 'day'));
-        assert.equal(result, '2024-07-30T23:59:59.999+01:00');
+        assert.equal(result, '2024-01-02T23:59:59.999Z');
     });
 
     it('should expire license in 1 year from given date', () => {
         const result = calculateExpiryDate(new Date().toISOString(), moment.duration(1, 'year'));
-        assert.equal(result, '2025-07-28T23:59:59.999+01:00');
+        assert.equal(result, '2024-12-31T23:59:59.999Z');
     });
 
     it('should get correct expiry when issue data is a date string', () => {
         const result = calculateExpiryDate(new Date('2018-01-01').toISOString(), moment.duration(2, 'years'));
-        assert.equal(result, '2019-12-31T23:59:59.999+00:00');
+        assert.equal(result, '2019-12-31T23:59:59.999Z');
     });
 });
