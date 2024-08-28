34a432a8fdaef8aadd655598e22d77d8f544586d
Kiran Varma <Kiran.Varma@digital.homeoffice.gov.uk>
2024-08-28 16:02:38 +0100

    feat: [ASSB-1391] UTC bug fix (#289)

--- test/helpers/calculate-expiry-date.js
+++ test/helpers/calculate-expiry-date.js
@@ -23,26 +23,26 @@ describe('calculateExpiryDate', () => {
 
     it('should expire license in 2 days from given date', () => {
         const result = calculateExpiryDate(undefined, moment.duration(2, 'day'));
-        assert.equal(result, '2024-01-02T23:59:59.999Z');
+        assert.equal(result, '2024-01-02T22:59:59.999Z');
     });
 
     it('should expire license in 1 year from given date', () => {
         const result = calculateExpiryDate(new Date().toISOString(), moment.duration(1, 'year'));
-        assert.equal(result, '2024-12-31T23:59:59.999Z');
+        assert.equal(result, '2024-12-31T22:59:59.999Z');
     });
 
     it('should expire license in 1 month from given date', () => {
         const result = calculateExpiryDate(new Date().toISOString(), { months: 1 });
-        assert.equal(result, '2024-01-31T23:59:59.999Z');
+        assert.equal(result, '2024-01-31T22:59:59.999Z');
     });
 
     it('should get correct expiry when issue data is a date string', () => {
         const result = calculateExpiryDate(new Date('2018-01-01').toISOString(), moment.duration(2, 'years'));
-        assert.equal(result, '2019-12-31T23:59:59.999Z');
+        assert.equal(result, '2019-12-31T22:59:59.999Z');
     });
 
     it('should get default expiry when issue data and duration are undefined', () => {
         const result = calculateExpiryDate(undefined, undefined);
-        assert.equal(result, '2028-12-31T23:59:59.999Z');
+        assert.equal(result, '2028-12-31T22:59:59.999Z');
     });
 });
